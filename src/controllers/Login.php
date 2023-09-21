820ec26da482f23a50bafd2f9c8c558b0d87dcea
standoge <stanlydoge@gmail.com>
2023-09-21 20:20:41 +0000

    scaffolding login code

--- src/controllers/Login.php
+++ src/controllers/Login.php
@@ -1,6 +1,20 @@
 <?php
 
 session_start();
-$con = mysqli_connect("", "", "", "") or die ("Unable to connect");
+$conn = mysqli_connect("", "", "", "") or die("Unable to connect");
 
+if (isset($POST["login"])) {
+	$user = $POST["username"];
+	$password = $POST["password"];
 
+	$query = "select * from usuarios where usuario = $user and contrasenia = $password";
+	$rs = mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($rs);
+
+	if (is_array($row)) {
+		$_SESSION["username"] = $row["usuario"];
+		$_SESSION["password"] = $row["contrasenia"];
+	} else {
+		echo "Error in login";
+	}
+}
