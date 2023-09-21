1e8a161b87ed00a86e101b66039b1b0c37bed58e
standoge <stanlydoge@gmail.com>
2023-09-20 22:16:52 -0600

    remove sample code

--- src/views/home.php
+++ src/views/home.php
@@ -1,10 +1,1 @@
-<?php include 'layout.php'; ?>
-
-<?php startblock('title') ?>
-    Home Page
-<?php endblock() ?>
-
-<?php startblock('content') ?>
-    <h1>Welcome to my PHP Application!</h1>
-    <p>This is the home page.</p>
-<?php endblock() ?>
+<?php 
--- src/views/layout.php
+++ src/views/layout.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>My PHP App</title>
-	<link rel="stylesheet" href="/css/style.css">
+    <meta charset="UTF-8">
+    <title>My PHP App</title>
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-	<header>
-		<nav>
-			<ul>
-				<li><a href="/">Home</a></li>
-			</ul>
-		</nav>
-	</header>
-	<main>
-		<?php include $content; ?>
-	</main>
-	<footer>
-		<p>&copy; 2021 My PHP App</p>
-	</footer>
-	<script src="/js/script.js"></script>
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+</main>
+<footer>
+    <p>&copy; 2021 My PHP App</p>
+</footer>
+<script src="/js/script.js"></script>
 </body>
 </html>
