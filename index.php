fe5f17791609288a9c3cbb3f983f3d7edfd98e0a
standoge <stanlydoge@gmail.com>
2023-10-01 22:27:40 +0000

    login controller n index for landing page

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// Load the Composer autoloader
+// require __DIR__ . '/../vendor/autoload.php';
+
+// Create a new instance of the router
+// $router = new \Bramus\Router\Router();
+
+// Define the root route
+// $router->get('/', function () {
+    // Load the home view
+    // require __DIR__ . '/../src/views/home.php';
+// });
+
+// Run the router
+// $router->run();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="src/controllers/LoginController.php">Login form</a> 
+</body>
+</html>
