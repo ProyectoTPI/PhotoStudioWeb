914c00673ef02b35c14d95691e4db089d09461a9
standoge <stanlydoge@gmail.com>
2023-10-02 21:15:06 -0600

    remove comments

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -20,7 +20,6 @@ session_start();
 
 		<?php
 
-		// $conn = mysqli_connect("", "", "", "") or die("Unable to connect");
 
 		if (isset($_POST["login"])) {
 			$user = $_POST["username"];
@@ -28,16 +27,6 @@ session_start();
 
 			echo "Session iniciada $user $password";
 
-			// $query = "select * from usuarios where usuario = $user and contrasenia = $password";
-			// $rs = mysqli_query($conn, $query);
-			// $row = mysqli_fetch_array($rs);
-
-			// if (is_array($row)) {
-			// 	$_SESSION["username"] = $row["usuario"];
-			// 	$_SESSION["password"] = $row["contrasenia"];
-			// } else {
-			// 	echo "Error in login";
-			// }
 		} else if (!empty($_POST["login"])) {
 			echo "Someting went wrong";
 		}
