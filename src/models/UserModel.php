867295d1d684adf601aa6d9c49a7b18b0f7b6d70
standoge <stanlydoge@gmail.com>
2023-10-06 00:53:18 +0000

    user as key for login query

--- src/models/UserModel.php
+++ src/models/UserModel.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once("Connection.php");
 
 class UserModel extends Connection
@@ -14,14 +13,14 @@ class UserModel extends Connection
 		$this->conn = $this->conn->getConnection();
 	}
 
-	public function login($username, $password)
+	public function login($username)
 	{
-		$query_parameters = [$username, $password];
+		$query_parameters = [$username];
 
 		$connection_obj = new Connection();
 		$test_connection = $connection_obj->getConnection();
 
-		$query = "select * from usuarios where usuario = ? and contrasenia = ?";
+		$query = "select * from usuarios where usuario = ?";
 		$cursor = $test_connection->prepare($query);
 		$cursor->execute($query_parameters);
 		$re = $cursor->fetch(PDO::FETCH_ASSOC);
