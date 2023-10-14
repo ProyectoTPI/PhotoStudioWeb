c2488ebf09b070aa70a0232dbbf47a5dd19bcb97
standoge <stanlydoge@gmail.com>
2023-10-13 22:37:37 -0600

    remove comments

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-// Load the Composer autoloader
-// require __DIR__ . '/../vendor/autoload.php';
-
-// Create a new instance of the router
-// $router = new \Bramus\Router\Router();
-
-// Define the root route
-// $router->get('/', function () {
-// Load the home view
-// require __DIR__ . '/../src/views/home.php';
-// });
-
-// Run the router
-// $router->run();
 ?>
 <!DOCTYPE html>
 <html lang="en">
