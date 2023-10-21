2e61dfccc472377ce0d6ff862d7baf4e805f7a5f
lmerinodev <admin_dge@DGEHM-25952.dgehm.gob.sv>
2023-10-21 00:06:45 -0600

    Creacion de plantilla para componentes de administracion) | creacion de styleHome

--- src/views/home.php
+++ src/views/home.php
@@ -8,7 +8,6 @@
     <title>.:PHOTOSTUDIOWEB - HOME:.</title>
     <link rel="stylesheet" href="./css/styleHome.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>-->
 </head>
 
 <body>
@@ -23,12 +22,12 @@
         </header>
         <ul>
             <li><a href="#"><i class="fa fa-home"></i>Inicio</a></li>
-            <li><a href="#"><i class="fa fa-shopping-bag"></i>Identidad empresarial</a></li>
+            <li><a href="#"><i class="fa fa-diamond"></i>Identidad empresarial</a></li>
             <li><a href="#"><i class="fa fa-calendar"></i>Agenda</a></li>
-            <li><a href="#"><i class="fa fa-link"></i>Empleados</a></li>
-            <li><a href="#"><i class="fa fa-cog"></i>Directorio telefónico</a></li>
-            <li><a href="#"><i class="fa fa-question-circle-o"></i>Dashboard</a></li>
-            <li><a href="#"><i class="fa fa-envelope-o"></i>Sobre nosotros</a></li>
+            <li><a href="#"><i class="fa fa-user"></i>Empleados</a></li>
+            <li><a href="#"><i class="fa fa-address-book-o"></i>Directorio telefónico</a></li>
+            <li><a href="#"><i class="fa fa-bar-chart"></i>Dashboard</a></li>
+            <li><a href="#"><i class="fa fa-info-circle"></i>Sobre nosotros</a></li>
         </ul>
     </section>
 
