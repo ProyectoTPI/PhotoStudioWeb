8520aa1265ad43c04b6e04339bb0bd9db9122419
Natanael555 <natanael.sanchez2002@gmail.com>
2023-10-23 21:40:07 -0600

    Feat: Responsive views
    Feat: The register user and appointment views, as well as the login view, became adaptable or responsive.

--- src/views/RegisterUser.php
+++ src/views/RegisterUser.php
@@ -22,39 +22,39 @@
         <form action="/src/controllers/RegController.php" method="post">
 
 
-        <div class="row m-1 mb-3">
-         <div class="form-group col-6">
+        <div class="row m-lg-1">
+         <div class="form-group col-md-6 col-form-label">
             <label for="name">Nombres</label>
             <input type="name" class="form-control" name="name" id="name" placeholder="Ingrese su nombre completo" Required>
             <small id="usernameHelp" class="form-text text-muted">Algunos ejemplos son: Juan Jose, Alex Antonio, etc.</small>
           </div>
         
-           <div class="form-group col-6">
+           <div class="form-group col-md-6 col-form-label">
             <label for="lastname">Apellidos</label>
             <input type="text" class="form-control" name="lastname" id="lastname" placeholder="Ingrese sus apellidos" Required>
-            <small id="lastnameHelp" class="form-text text-muted">Algunos ejemplos son: Rodríguez, González, Fernández, etc.</small>
+            <small id="lastnameHelp" class="form-text text-muted">Algunos ejemplos son: Rodríguez, González, etc.</small>
           </div>
         </div>
 
-        <div class="row m-1 mb-3">
-          <div class="form-group col-6">
-              <label for="dui">DUI:</label>
+        <div class="row m-lg-1">
+          <div class="form-group col-md-6 col-form-label">
+              <label for="dui">DUI</label>
               <input type="text" class="form-control" id="dui" name="dui" placeholder="Ingrese su numero de Documento Único de Identidad" Required>
             </div>
-            <div class="form-group col-6">
+            <div class="form-group col-md-6 col-form-label">
               <label for="numberphone">Teléfono</label>
               <input type="text" class="form-control" id="telefono" name="numberphone" placeholder="Ingrese su numero de teléfono" Required>
             </div>
         </div>
 
-        <div class="row m-1 mb-3">
-          <div class="form-group col-6">
+        <div class="row m-lg-1">
+          <div class="form-group col-lg-6  col-form-label">
               <label for="username">Nombre de usuario</label>
               <input type="text" class="form-control" name="username" id="username" placeholder="Ingrese el nombre de usuario" required>
               <small id="usernameHelp" class="form-text text-muted">Algunos ejemplos son: lmerino, luis.merino etc.</small>
             </div>
             
-            <div class="form-group col-6">
+            <div class="form-group col-lg-6 col-form-label">
               <label for="email">Dirección de correo electrónico</label>
               <input type="email" class="form-control" id="email" name="email" placeholder="Ingrese su correo electrónico" Required>
               <small id="emailHelp" class="form-text text-muted">Nunca compartiremos tu correo electrónico con nadie
@@ -62,20 +62,24 @@
             </div>
         </div>
         
-        <div class="row m-1">
-          <div class="form-group col-6 prueba3">
+        <div class="row m-lg-1">
+          <div class="form-group col-lg-6 col-form-label">
               <label for="password">Contraseña</label>
               <input type="password" class="form-control" id="password" name="password" placeholder="Ingrese la contraseña" Required>
             </div>
-            <div class="form-group col-6">
+            <div class="form-group col-lg-6 col-form-label">
               <label for="password2">Confirmar contraseña</label>
               <input type="password" class="form-control" id="password2" name="password2" placeholder="Ingrese la contraseña de nuevo" Required>
             </div> 
         </div>
-          
-          <div class="mt-5 m-2 d-flex justify-content-center">
+
+        <div class="m-lg-3">
+          <div class="form-margint d-flex justify-content-center">
             <button type="submit" class="btn btn-lg btn-primary px-5">Registrarte</button>
           </div>
+        </div>  
+         
+
           <div class="d-flex justify-content-center form-margint">
             <p>¿Usted tiene una cuenta? <a href="login.php">Iniciar sesión</a></p>
           </div>
